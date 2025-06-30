d087e7c135dc85d705fa652186deefe673c75108
Mari <shahdabolatif@gmail.com>
2025-06-30 08:56:05 -0400

    FrontEnd npm update

--- public/build/assets/CardDescription.vue_vue_type_script_setup_true_lang-DyGu-_SZ.js
+++
@@ -1,6 +0,0 @@
-import{a as t,b as o}from"./AppLogoIcon.vue_vue_type_script_setup_true_lang-Cn4VzyVq.js";import{d as r,c as n,o as c,n as l,u as p,r as d}from"./app-CpWbJJER.js";/**
- * @license lucide-vue-next v0.468.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const f=t("FileIcon",[["path",{d:"M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z",key:"1rqfz7"}],["path",{d:"M14 2v4a2 2 0 0 0 2 2h4",key:"tnqrlb"}]]),_=r({__name:"CardDescription",props:{class:{}},setup(a){const e=a;return(s,i)=>(c(),n("p",{"data-slot":"card-description",class:l(p(o)("text-muted-foreground text-sm",e.class))},[d(s.$slots,"default")],2))}});export{f as F,_};
